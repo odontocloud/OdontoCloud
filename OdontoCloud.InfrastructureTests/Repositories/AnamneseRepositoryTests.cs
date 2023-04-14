4c1fddfcec9a43f81181a0cc35e20e3aacea1ddb
odontoCloud <odontocloud@hotmail.com>
2023-04-14 15:36:00 -0300

    Adicionando classe util para testes

--- OdontoCloud.InfrastructureTests/Repositories/AnamneseRepositoryTests.cs
+++ OdontoCloud.InfrastructureTests/Repositories/AnamneseRepositoryTests.cs
@@ -1,24 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OdontoCloud.Domain.Entities;
 using OdontoCloud.Infrastructure.Context;
+using OdontoCloud.InfrastructureTests.Utilitaries;
 using Xunit;
 
 namespace OdontoCloud.Infrastructure.Repositories.Tests
 {
     public class AnamneseRepositoryTests
     {
-        private static OdontoCloudDBContext GetDbContextInMemory()
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<OdontoCloudDBContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
-            var context = new OdontoCloudDBContext(optionsBuilder.Options);
-            return context;
-        }
-
         [Fact]
         public void AddTest()
         {
             //Arrange
-            var repository = new AnamneseRepository(GetDbContextInMemory());
+            var repository = new AnamneseRepository(OdontoCloudUtil.GetDbContextInMemory());
             Anamnese anamnese = new Anamnese(null, 1, false, "Descriçao", false, false, false, false, false, false, false, false, false, "Descrição", false, "Descrição", false, false, false, false, "Descrição", false, false);
 
             //Act
